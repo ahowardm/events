2fdf7bbe4a783532bc6d5d833c31646b949de6aa
Andrés Howard <ahowardm@gmail.com>
2023-05-03 17:19:50 -0400

    feat: add event seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Category.delete_all
 Venue.delete_all
+Event.delete_all
 
 Category.create! [
     { name: 'Concierto' },
@@ -19,4 +20,10 @@ Venue.create! [
     {name: 'Estadio San Carlos', address: 'San Carlos de Apoquindo 2200', capacity: 15000},
     {name: 'Estadio Nacional', address: 'Avenida Grecia s/n', capacity: 10000},
     {name: 'Parque Intercomunal de la Reina', address: 'Avenida Francisco de Bilbao 8105', capacity: 35000}
+]
+
+Event.create! [
+    {name: 'Rod Stewart', date: Date.today + 100.days, venue: Venue.first, category: Category.first },
+    {name: 'Bad Bunny', date: Date.today + 15.days, venue: Venue.second, category: Category.first },
+    {name: 'Campeonato mundial de trial', date: Date.today + 55.days, venue: Venue.third, category: Category.third }
 ]
