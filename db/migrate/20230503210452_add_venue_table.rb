be25b3be4081d1d48f286e8760c716487137795a
Andrés Howard <ahowardm@gmail.com>
2023-05-03 17:06:27 -0400

    feat: add venue table

---
+++ db/migrate/20230503210452_add_venue_table.rb
@@ -0,0 +1,9 @@
+class AddVenueTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :venues do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.bigint :capacity, null: false, default: 0
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_210031) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_210452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.bigint "capacity", default: 0, null: false
   end
 
 end
