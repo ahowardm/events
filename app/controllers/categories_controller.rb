cae4fbb0a620987920bb519c76bd605f50dd9bb5
Andrés Howard <ahowardm@gmail.com>
2023-05-03 21:55:13 -0400

    feat: add form to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
 class CategoriesController < ApplicationController
+    def new
+        @category = Category.new
+    end
+
     def index
         @categories = Category.all
     end
@@ -7,4 +11,18 @@ class CategoriesController < ApplicationController
         @category = Category.find params[:id]
         @events = @category.events
     end
+
+    def create
+        @category = Category.new category_params
+        if @category.save
+            redirect_to @category, notice: 'Categoría creada con éxito'
+        else
+            redirect_to new_category_path, alert: 'No ha sido posible crear la categoría'
+        end
+    end
+
+    private
+        def category_params
+            params.require(:category).permit(:name)
+        end
 end
