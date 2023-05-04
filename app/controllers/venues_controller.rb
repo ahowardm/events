b912a9bb6afc782101b5f50aca8b982a417d878f
Andrés Howard <ahowardm@gmail.com>
2023-05-03 22:18:40 -0400

    feat: enable edition and deletion of venues

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,4 +1,8 @@
 class VenuesController < ApplicationController
+    def new
+        @venue = Venue.new
+    end
+
     def index
         @venues = Venue.all
     end
@@ -7,4 +11,37 @@ class VenuesController < ApplicationController
         @venue = Venue.find params[:id]
         @events = @venue.events
     end
+
+    def edit
+        @venue = Venue.find params[:id]
+    end
+
+    def create
+        @venue = Venue.new venue_params
+        if @venue.save
+            redirect_to @venue, notice: 'Locación creada con éxito'
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @venue = Venue.find params[:id]
+        if @venue.update venue_params
+            redirect_to @venue, notice: 'Locación actualizada con éxito'
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @venue = Venue.find params[:id]
+        @venue.destroy
+        redirect_to venues_path
+    end
+
+    private
+        def venue_params
+            params.require(:venue).permit(:name, :address, :capacity)
+        end
 end
